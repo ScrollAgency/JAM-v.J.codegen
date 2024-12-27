cdbb4542595186b8c1e6e5d647d79bba2a3cf8f9
jeanscroll <jean@agence-scroll.com>
2024-12-27 16:02:43 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,6 @@ import '@/styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import * as React from "react";
-import { PlasmicCanvasHost, registerComponent } from "@plasmicapp/react-web/lib/host";
-// import TextInput from "@/components/text-input";
-import Button from "@/components/Button";
-// import TextLink from "@/components/text-link";
-import Switch from "@/components/switch";
-// import ProgressBar from "@/components/progress-bar";
-// import Checkbox from "@/components/checkbox";
-
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -19,42 +10,3 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </PlasmicRootProvider>
   );
 }
-
-// Register le composant Button
-registerComponent(Button, {
-  // Nom du composant côté Builder (CMS)
-  name: "Button",
-  // Propriétés du composant Button
-  props: {
-    onClick: {
-      type: "eventHandler",
-      argTypes: [],
-    },
-    text: {
-      type: "string",
-      defaultValue: "Text",
-      required: true,
-    },
-    desctructive: {
-      type: "boolean",
-      defaultValue: false,
-      required: false,
-    },
-  },
-  importPath: "@/components/button",
-});
-
-registerComponent(Switch, {
-  name: "Switch",
-  props: {
-    status: {
-      type: "choice",
-      options: ["Enabled", "Disabled"],
-    },
-    state: {
-      type: "choice",
-      options: ["Selected", "Unselected"],
-    },
-  },
-  importPath: "@/components/switch",
-});
